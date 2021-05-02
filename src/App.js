842e0c18fd5b47b8c4a7f9a40c165f96fd780aa1
rakesh <rakesh.sahu694@gmail.com>
2021-05-03 00:12:27 +0530

    Update App.js
    commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Container } from 'semantic-ui-react';
 import './App.css';
 import DisplayBalance from './components/DisplayBalance';
@@ -7,6 +8,7 @@ import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries)
   return (
     <Container>
       <MainHeader
@@ -15,7 +17,7 @@ function App() {
       <DisplayBalance title="Your Balance" value="2550.544" color="green" size="small" />
       <DisplayBalances />
       <MainHeader title="History" type="h3" />
-      <EntryLine description="income" value="10.00" />
+      <EntryLine description={entries[0].description} value={entries[0].value} isExpense={entries[0].isExpense}/>
       <EntryLine description="expense" value="10.00" isExpense />
       <EntryLine description="income" value="20.00" />
       <EntryLine description="expense" value="10.00" isExpense />
@@ -27,3 +29,28 @@ function App() {
 }
 
 export default App;
+
+
+var initialEntries = [
+  {
+    description:"Work Income",
+    value:"1000.00",
+    isExpense:false
+  },
+  {
+    description:"Water Bill",
+    value:"20.00",
+    isExpense:true
+  },
+  {
+    description:"Rent",
+    value:"200.00",
+    isExpense:true
+  },
+  {
+    description:"Power bill",
+    value:"50.00",
+    isExpense:true
+  }
+
+]
